1a78cc829c1489a21f76a5f53042fd7b903c79bd
Yishay Garame <54310381+YishayGarame@users.noreply.github.com>
2025-11-17 18:19:43 +0200

    adding catalog feature with mediatr carter and mapster

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,43 @@
+﻿namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+    : ICommand<CreateProductResult>;
+public record CreateProductResult(Guid Id);
+
+public class CreateProductCommandValidatior : AbstractValidator<CreateProductCommand>
+{
+    public CreateProductCommandValidatior()
+    {
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required!");
+        RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required!");
+        RuleFor(x => x.ImageFile).NotEmpty().WithMessage("ImageFile is required!");
+        RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0");
+    }
+}
+
+internal class CreateProductCommandHandler
+    (IDocumentSession session,IValidator<CreateProductCommand> validator)
+    :ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        // create a product entity from command object
+
+        var product = new Product
+        {
+            Name = command.Name, 
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+
+        // Save to database
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
+
+        // return CreateProductResult result
+        return new CreateProductResult(product.Id);
+    }
+}
+
---
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,24 @@
+using BuildingBlocks.Behaviors;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+});
+
+builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
+
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapCarter();
+
+app.Run();
